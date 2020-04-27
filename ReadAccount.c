889cbe14b97c0743ab3408a5610661feb7770aba
prel <black7321@naver.com>
2020-04-27 17:18:48 +0900

    파일에서 불러오고 저장만 수행. 출력은 하지 않음

---
+++ ReadAccount.c
@@ -0,0 +1,70 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include "header.h"
+#define MAX_LINE_LENGTH 130
+
+Account readAccountInfo(int line) {
+	FILE* fp;
+	fp = fopen("./accountlist.txt", "r");
+	if (fp == NULL)
+	{
+		printf("파일 읽기 실패\n");
+		exit(1);
+	}
+	char temp[MAX_LINE_LENGTH];
+	int lineCounter = 1;
+	while (lineCounter != line) {
+		fgets(temp, MAX_LINE_LENGTH, fp);
+		lineCounter++;
+
+	}
+
+	Account a = { "", "", "", "", "", {0, }, {0, }, 0, 0, 0 };
+	int i = 0;
+	while (true) {
+		a.id[i] = fgetc(fp);
+		if (a.id[i] == ',') { a.id[i] = '\0'; i = 0; break; }
+		i++;
+	}
+	while (true) {
+		a.pw[i] = fgetc(fp);
+		if (a.pw[i] == ',') { a.pw[i] = '\0'; i = 0; break; }
+		i++;
+	}
+	while (true) {
+		a.name[i] = fgetc(fp);
+		if (a.name[i] == ',') { a.name[i] = '\0'; i = 0; break; }
+		i++;
+	}
+	while (true) {
+		a.nick[i] = fgetc(fp);
+		if (a.nick[i] == ',') { a.nick[i] = '\0'; i = 0; break; }
+		i++;
+	}
+	while (true) {
+		a.email[i] = fgetc(fp);
+		if (a.email[i] == ',') { a.email[i] = '\0'; i = 0; break; }
+		i++;
+	}
+	while (true) {
+		int temp = fgetc(fp) - 48;
+		if (temp != -4)
+		{ a.birth[i] = temp; i++; }
+		else { i = 0; break; }
+	}
+	while (true) {
+		int temp = fgetc(fp) - 48;
+		if (temp != -4) { a.phone[i] = temp; i++; }
+		else { i = 0; break; }
+	}
+
+	fscanf(fp, "%d", &a.rec);
+	fgetc(fp);
+	a.lock = (fgetc(fp) - 48);
+	fgetc(fp);
+	a.admin = (fgetc(fp) - 48);
+
+	fclose(fp);
+
+	return a;
+}
