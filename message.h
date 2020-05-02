973dc45c3258cd11a58849bb3ef5d02b9d4f6c0a
Jaehun-Jo <58066704+jaehoon-jo@users.noreply.github.com>
2020-05-02 18:02:36 +0900

    Update message.h

--- message.h
+++ message.h
@@ -1,10 +1,19 @@
 #pragma once
 #define ID_MAXSIZE 12
-#define text_MAXSIZE 200
+#define TEXT_MAXSIZE 200
 
 typedef struct Message {
 	char senderId[ID_MAXSIZE + 2];
 	char receiverId[ID_MAXSIZE + 2];
 	bool read;
-	char text[text_MAXSIZE + 2];
+	char text[TEXT_MAXSIZE + 2];
 } Message;
+int countMessage(char* id);
+Message* readMessage(char* id);
+bool matchMessage(const char* string);
+char* writeMessage();
+void sendMessage(char* sender, char* receiver);
+void deleteMessage(Message* m);
+void showMessage(Message* m, char* id, int num);
+Message* showMessageList(char* id);
+void messageBox(char* id);
