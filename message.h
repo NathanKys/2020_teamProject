966fba84ef6de66c1fcfa3c37a394edfba5c1416
Jaehun-Jo <58066704+jaehoon-jo@users.noreply.github.com>
2020-05-03 21:30:21 +0900

    Update message.h

--- message.h
+++ message.h
@@ -3,8 +3,8 @@
 #define TEXT_MAXSIZE 200
 
 typedef struct Message {
-	char senderId[ID_MAXSIZE + 2];
 	char receiverId[ID_MAXSIZE + 2];
+	char senderId[ID_MAXSIZE + 2];
 	bool read;
 	char text[TEXT_MAXSIZE + 2];
 } Message;
@@ -13,8 +13,9 @@ Message* readMessageFile(char* id, int count);
 bool matchMessage(const char* string);
 char* writeMessage();
 void sendMessage(char* sender, char* receiver);
-void deleteMessage(Message* m);
-void showMessage(Message* m, char* id, int num);
+void showMessage(Message* m, char* id, int count, int index);
 Message* showMessageList(char* id);
+int findLine(char* id, int count, int flag);
+void deleteMessage(char* id, int count, int index);
+void readMessage(char* id, int count, int index);
 void messageBox(char* id);
-void readMessage(const char*)
