e512d5269414c9b1a0ecc7e07e60371755584c05
prel <black7321@naver.com>
2020-05-04 17:10:19 +0900

    로그인 제대로 돌아가도록 수정

--- Login.c
+++ Login.c
@@ -5,13 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char admin_id[] ="1234";
-char admin_password[] ="1234";
+char admin_id[] = "1234";
+char admin_password[] = "1234";
 
 int loginFunction(int* num_account) {
 
 	FILE* memlist;
-	
+
 	memlist = fopen("./accountlist.txt", "r");
 	if (memlist == NULL)
 	{
@@ -53,7 +53,7 @@ int loginFunction(int* num_account) {
 
 		}
 	}
-	
+
 	int row = 0;
 	char stdid[14];
 	char stdpassword[18];
@@ -64,45 +64,53 @@ int loginFunction(int* num_account) {
 		fgets(stdid, 14, stdin);
 
 		// 배열 크기가 넘어가면 입력 버퍼를 비움.
-
 		stdid[strcspn(stdid, "\n")] = 0;
-		
 		if (strlen(stdid) > 12) {
 			while (getchar() != '\n');
 		}
 		for (int i = 0; i < *num_account; i++) {       //i<num_account 로 수정
 			if (strcmp(account_id[i], stdid) == 0) {
+			pass:system("cls");
+				char stdpassword[18] = "";
+				printf("비밀번호를 입력하세요:");
+				fgets(stdpassword, 18, stdin);
 
-				
-				pass:system("cls");
-					 printf("비밀번호를 입력하세요:");
-					fgets(stdpassword, 18, stdin);
-					if (strlen(stdid) > 12) {
-						while (getchar() != '\n');
-					}
-					stdpassword[strcspn(stdpassword, "\n")] = 0;
-					if (strcmp(account_password[i], stdpassword) == 0) {
-						printf("\n로그인 성공\n");
-						system("pause");
-						row = i + 1;
+				char c = ' ';
+				while (stdpassword[16]!= 0) {
+					scanf("%c", &c);
+					if (c == '\n')
 						break;
-					}
-					else {
-						printf("비밀번호가 틀립니다.\n");
-						system("pause");
-						goto pass;
-					}
+				}
+				stdpassword[strcspn(stdpassword, "\n")] = 0;
 				
+				if (strcmp(account_password[i], stdpassword) == 0) {
+					
+					printf("로그인 성공\n");
+					system("pause");
+					row = i + 1;
+					break;
+				}
+				else {
+					printf("비밀번호가 틀립니다.\n");
+					system("pause");
+					goto pass;
+				}
+
 			}
 			else if (strcmp(admin_id, stdid) == 0) {
-				pass_admin: printf("관리자 비밀번호를 입력해주세요:");
+			pass_admin: system("cls"); 
+				printf("관리자 비밀번호를 입력해주세요:");
+				char stdpassword[18] = "";
 				fgets(stdpassword, 18, stdin);
-				if (strlen(stdid) > 12) {
-					while (getchar() != '\n');
+
+				char c = ' ';
+				while (stdpassword[16] != 0) {
+					scanf("%c", &c);
+					if (c == '\n')
+						break;
 				}
 				stdpassword[strcspn(stdpassword, "\n")] = 0;
 				if (strcmp(admin_password, stdpassword) == 0) {
-					printf("\n로그인 성공\n");
 					row = -1;
 					break;
 				}
